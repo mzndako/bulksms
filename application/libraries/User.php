f2f28d6355b8ce56168f85dab93e55a13f24e4c4
Ndako Muhammad Z <mzndako@gmail.com>
2019-08-20 11:22:45 +0100

    Remove method having class name

--- application/libraries/User.php
+++ application/libraries/User.php
@@ -105,7 +105,7 @@ class User {
 	var $token = "";
 	public $user_count = 0;
 
-	function User($user=0, $columns="id") {
+	function __construct($user=0, $columns="id") {
 		if(!empty($user))
 			$this->set_user($user, $columns);
 	}
